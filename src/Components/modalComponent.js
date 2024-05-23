02c1d93d876a908bcbea7aa31a280b6a5bc35a23
RichardFlor <richardflor2015@gmail.com>
2024-05-22 23:04:49 -0300

    modalComponent integrado com o backend

--- src/Components/modalComponent.js
+++ src/Components/modalComponent.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal, Box, Typography, TextField } from "@mui/material";
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
@@ -12,35 +13,92 @@ const style = {
   p: 4,
 };
 
-const mockUser = {
-  name: "Felipe Richard Carmino",
-  email: "CarminoFelipinho@AnckieDuckie.com",
-  password: "TesteJhon" // demo de senha byjhonzitos
-};
-
 function ModalComponent() {
   const [open, setOpen] = useState(false);
-  const [user, setUser] = useState(mockUser);
+  const [user, setUser] = useState({});
   const [editMode, setEditMode] = useState(false);
   const [editedUser, setEditedUser] = useState({});
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const handleOpen = () => {
+  useEffect(() => {
+    if (error || success) {
+      const timer = setTimeout(() => {
+        setError("");
+        setSuccess("");
+      }, 3000); // Mensagem desaparecerá após 3 segundos
+
+      return () => clearTimeout(timer);
+    }
+  }, [error, success]);
+
+  const handleOpen = async () => {
     setOpen(true);
-    setEditedUser({ ...user }); // Copiando os dados do usuário para serem editados
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setError("Token não encontrado");
+      return;
+    }
+
+    try {
+      const response = await axios.get("http://localhost:8080/user/read", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(response.data); // Verificar os dados recebidos do backend
+      setUser(response.data);
+      setEditedUser(response.data);
+    } catch (error) {
+      setError("Erro ao carregar os dados do usuário");
+      console.error("Erro ao carregar os dados do usuário:", error);
+    }
   };
 
   const handleClose = () => {
     setOpen(false);
     setEditMode(false);
+    setError("");
+    setSuccess("");
   };
 
   const handleEdit = () => {
     setEditMode(true);
   };
 
-  const handleSave = () => {
-    setUser(editedUser); // Salvando as alterações
-    setEditMode(false);
+  const handleSave = async () => {
+    if (!editedUser.nome || !editedUser.email || !editedUser.password) {
+      setError("Todos os campos são obrigatórios");
+      setSuccess(""); // Clear success message when there is an error
+      return;
+    }
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setError("Token não encontrado");
+      return;
+    }
+
+    try {
+      const response = await axios.put("http://localhost:8080/user/update", {
+        nome: editedUser.nome,
+        email: editedUser.email,
+        password: editedUser.password,
+      }, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log("Atualização realizada com sucesso:", response.data);
+      setUser(editedUser); // Salvando as alterações no estado do usuário
+      setEditMode(false);
+      setSuccess("Atualizado com sucesso!");
+      setError("");
+    } catch (error) {
+      console.log("Erro durante a atualização:", error);
+      setSuccess(""); // Clear success message when there is an error
+      if (error.response && error.response.status === 404) {
+        setError("Usuário não encontrado");
+      } else {
+        setError(error.message || "Erro ao atualizar cadastro");
+      }
+    }
   };
 
   const handleChange = (e) => {
@@ -66,10 +124,12 @@ function ModalComponent() {
           <Typography id="modal-title" variant="h6" component="h2">
             Perfil do Anckier
           </Typography>
+          {error && <Typography color="error">{error}</Typography>}
+          {success && <Typography color="success">{success}</Typography>}
           <TextField
             label="Nome"
-            name="name"
-            value={editMode ? editedUser.name : user.name}
+            name="nome"
+            value={editMode ? editedUser.nome : user.nome}
             onChange={handleChange}
             fullWidth
             margin="normal"
@@ -83,17 +143,17 @@ function ModalComponent() {
             onChange={handleChange}
             fullWidth
             margin="normal"
-            disabled={!editMode} // Campo email + edit
+            disabled// Campo email + edit
           />
           <TextField
             label="Senha"
             name="password"
             type="password"
-            value={editMode ? editedUser.password : user.password}
+            //value={editMode ? editedUser.password : user.password}
             onChange={handleChange}
             fullWidth
             margin="normal"
-            disabled={!editMode} // Campo senha + edit, notem que otipo é password para ficar *****
+            disabled={!editMode} // Campo senha + edit, notem que o tipo é password para ficar *****
           />
           {!editMode ? (
             <Button 
