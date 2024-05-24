444f785e706e7113365c691cde3e43a533c98a8e
Felipe <koutodev@gmail.com>
2024-05-24 11:06:55 -0300

    remoção de rotas de forgetPassword

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import Home from "./Pages/home";
 import HomeUser from "./Pages/homeUser";
 import Login from "./Pages/login";
 import Register from "./Pages/register";
-import Forgot from "./Pages/forgotPassword";
 import Profile from "./Pages/profile";
 import { Gemini } from "./Pages/Gemini";
 
@@ -16,7 +15,6 @@ function AppRoutes() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
-          <Route path="/forgot" element={<Forgot />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/homeuser" element={<HomeUser />}></Route>
           <Route path="/chat" element={<Gemini />}></Route>
