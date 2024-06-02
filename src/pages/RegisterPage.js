a561020f50cdf69953a31eeadb1ea338e1549504
Felipe <koutodev@gmail.com>
2024-06-01 22:19:37 -0300

    [Felipe Couto] - Adição de comentários, organização dos imports, melhoria do layout e adição de retorno de erros

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,78 +1,80 @@
 import React, { useState, useEffect } from "react";
+// Router DOM
+import { Link, useNavigate } from "react-router-dom";
+// Axios (Comunicação Backend)
 import axios from "axios";
-import { Link } from "react-router-dom";
-// Body
+// MUI UI
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import TextField from "@mui/material/TextField";
-// Typography
+// import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-// Header
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
-import { alpha, styled } from "@mui/material/styles";
-// Images
-const Logo = require("../logo.svg").ReactComponent;
+// Cores
+import ColorsUse from "../components/Colors/Colors";
+// Animação
+import { motion } from "framer-motion";
+// Components
+import CustomTextField from "../components/MUICustom/TextField";
 
-// TextField Personalizado
-const DuckieTextField = styled((props) => (
-  <TextField InputProps={{ disableUnderline: true }} {...props} />
-))(({ theme }) => ({
-  "& .MuiFilledInput-root": {
-    overflow: "hidden",
-    borderRadius: "1.2rem",
-    backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
-    border: "1px solid",
-    borderColor: theme.palette.mode === "light" ? "#C6B4E4" : "#2D3843",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    "&:hover": {
-      backgroundColor: "#e5e5e5",
-      border: "1px solid #8C68C8",
-    },
-    "&.Mui-focused": {
-      backgroundColor: "#e5e5e5",
-      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main,
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
     },
   },
-}));
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+};
 
 export default function RegisterPage() {
+  const { primaryColor, primaryColorHover, primaryColorTransparent } =
+    ColorsUse();
   const [nome, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (error || success) {
+    if (error) {
       const timer = setTimeout(() => {
         setError("");
-        setSuccess("");
-      }, 3000); // Mensagem desaparecerá após 3 segundos
-
+      }, 9000);
       return () => clearTimeout(timer);
     }
-  }, [error, success]);
+  }, [error]);
 
   const handleSubscribe = async () => {
     if (!nome || !email || !password || !confirmPassword) {
-      setError("Todos os campos são obrigatórios");
-      setSuccess(""); // Clear success message when there is an error
+      setError("Preencha todos os campos.");
+      setSuccess("");
       return;
     }
 
     if (password !== confirmPassword) {
-      setError("As senhas não coincidem");
-      setSuccess(""); // Clear success message when there is an error
+      setError("Suas senhas não são iguais.Tente novamente com mais calma.");
+      setSuccess("");
+      return;
+    }
+
+    const allowedDomains = ["gmail.com", "icloud.com", "outlook.com"];
+    const emailRegex = /@([^\s@]+)$/; // Regex para pegar o domínio do email
+    const match = email.match(emailRegex);
+    if (!match || !allowedDomains.includes(match[1])) {
+      setError("Por favor, insira um e-mail com dóminio válido.");
+      setSuccess("");
       return;
     }
 
@@ -82,7 +84,7 @@ export default function RegisterPage() {
         email,
         password,
       });
-      console.log("Cadastro realizado com sucesso:", response.data);
+      console.log("Conta criada com sucesso:", response.data);
       const token = response.data?.token; // Certifique-se de que está acessando o campo correto
       if (token) {
         localStorage.setItem("token", token);
@@ -92,9 +94,22 @@ export default function RegisterPage() {
         setPassword("");
         setConfirmPassword("");
         setError("");
-        setSuccess("Cadastrado com sucesso!");
+
+        // Contador regressivo para redirecionamento
+        var countdown = 5;
+        const countdownInterval = setInterval(() => {
+          setSuccess(
+            `Cadastrado com sucesso! Redirecionando em ${countdown} segundos...`
+          );
+          countdown--;
+          if (countdown === 0) {
+            clearInterval(countdownInterval);
+            setSuccess(""); // Limpa a mensagem de sucesso após o redirecionamento
+            navigate("/home");
+          }
+        }, 1000);
       } else {
-        throw new Error("Token não encontrado na resposta");
+        throw new Error("Token não encontrado na resposta.");
       }
     } catch (error) {
       console.log("Erro durante a inscrição:", error);
@@ -107,164 +122,259 @@ export default function RegisterPage() {
     }
   };
 
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      handleSubscribe();
+    }
+  };
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container
-        sx={{
-          overflow: "hidden",
-          bgcolor: "#ADBDEB",
-          minWidth: "100vw !important",
-          margin: "0px !important",
-          padding: "0px !important",
-        }}
-      >
-        <Box
-          maxWidth="sm"
-          sx={{
-            height: "100vh",
-            margin: "auto",
-            padding: ".5rem",
-            alignContent: "center",
-          }}
-        >
-          {/* Header */}
-          <AppBar
-            position="absolute"
-            sx={{
-              position: "fixed",
-              top: "0",
-              background: "transparent",
-              boxShadow: "none",
-              color: "black",
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {error && (
+        <Box sx={{ position: "absolute", top: "10%" }}>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ rotate: 0, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
             }}
           >
-            <Toolbar sx={{ justifyContent: "space-between" }}>
-              <Box>
-                <Logo />
-              </Box>
-              <Box>
-                <Link to="/login">
-                  <Button
-                    variant="text"
-                    sx={{
-                      background: "linear-gradient(45deg, #20212B, #16171F)",
-                      padding: ".5rem 1rem",
-                      borderRadius: ".5rem",
-                      textTransform: "capitalize",
-                      color: "#FFF",
-                    }}
-                  >
-                    Login
-                  </Button>
-                </Link>
-              </Box>
-            </Toolbar>
-          </AppBar>
-
-          {/* Sucess / Error Box */}
-          <Box>
-            {error && (
+            <Container sx={{ width: "fit-content" }}>
               <Alert
                 severity="error"
                 sx={{
-                  fontWeight: "600",
+                  marginBottom: "1rem",
                   borderRadius: "1rem",
-                  mt: 1,
-                  mb: 1,
+                  border: "2px solid",
                 }}
               >
                 {error}
               </Alert>
-            )}
-            {success && (
+            </Container>
+          </motion.div>
+        </Box>
+      )}
+      {success && (
+        <Box sx={{ position: "absolute", top: "10%" }}>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ rotate: 0, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+            }}
+          >
+            <Container sx={{ width: "fit-content" }}>
               <Alert
                 severity="success"
                 sx={{
-                  fontWeight: "600",
+                  marginBottom: "1rem",
                   borderRadius: "1rem",
-                  mt: 1,
-                  mb: 1,
+                  border: "2px solid",
                 }}
               >
                 {success}
               </Alert>
-            )}
-          </Box>
-          {/* Login Form */}
-          <Box
-            sx={{
-              background: "#FFF",
-              padding: ".5rem",
-              borderRadius: "1.5rem",
-              textAlign: "center",
+            </Container>
+          </motion.div>
+        </Box>
+      )}
+      <Container component="section" maxWidth="xs">
+        <CssBaseline />
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <motion.div variants={item}>
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{ fontWeight: 600, padding: "1rem 0" }}
+            >
+              Criar Conta
+            </Typography>
+          </motion.div>
+          <motion.div
+            variants={container}
+            initial="hidden"
+            animate="visible"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: ".5rem",
+              width: "100%",
             }}
           >
-            <Box>
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{ color: "#000", padding: "1rem 0rem" }}
-              >
-                Criar uma conta
-              </Typography>
-            </Box>
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: ".5rem" }}
-            >
-              <DuckieTextField
-                id="outlined-required-nome"
-                label="Name"
+            {/* Input Nome */}
+            <motion.div variants={item}>
+              <CustomTextField
+                id="nome"
+                label="Nome"
                 variant="filled"
                 value={nome}
                 onChange={(e) => setName(e.target.value)}
-                sx={{ width: "100%" }}
+                onKeyDown={handleKeyDown}
+                sx={{
+                  width: "100%",
+                  "& .MuiFilledInput-root": {
+                    backgroundColor: primaryColorTransparent,
+                    borderColor: primaryColor,
+                    "&:hover": {
+                      backgroundColor: primaryColorTransparent,
+                      border: "1px solid",
+                      borderColor: primaryColorHover,
+                    },
+                    "&.Mui-focused": {
+                      backgroundColor: primaryColorTransparent,
+                      boxShadow: `${primaryColor} 2px 2px 15px`,
+                      borderColor: primaryColorHover,
+                    },
+                  },
+                }}
               />
-              <DuckieTextField
-                id="outlined-required-email"
+            </motion.div>
+            {/*  Input E-mail */}
+            <motion.div variants={item}>
+              <CustomTextField
+                id="email"
                 label="E-mail"
                 variant="filled"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                sx={{ width: "100%" }}
+                onKeyDown={handleKeyDown}
+                sx={{
+                  width: "100%",
+                  "& .MuiFilledInput-root": {
+                    backgroundColor: primaryColorTransparent,
+                    borderColor: primaryColor,
+                    "&:hover": {
+                      backgroundColor: primaryColorTransparent,
+                      border: "1px solid",
+                      borderColor: primaryColorHover,
+                    },
+                    "&.Mui-focused": {
+                      backgroundColor: primaryColorTransparent,
+                      boxShadow: `${primaryColor} 2px 2px 15px`,
+                      borderColor: primaryColorHover,
+                    },
+                  },
+                }}
               />
-              <DuckieTextField
+            </motion.div>
+            {/* Input Password */}
+            <motion.div variants={item}>
+              <CustomTextField
                 label="Senha"
                 type="password"
                 variant="filled"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                sx={{ width: "100%" }}
+                onKeyDown={handleKeyDown}
+                sx={{
+                  width: "100%",
+                  "& .MuiFilledInput-root": {
+                    backgroundColor: primaryColorTransparent,
+                    borderColor: primaryColor,
+                    "&:hover": {
+                      backgroundColor: primaryColorTransparent,
+                      border: "1px solid",
+                      borderColor: primaryColorHover,
+                    },
+                    "&.Mui-focused": {
+                      backgroundColor: primaryColorTransparent,
+                      boxShadow: `${primaryColor} 2px 2px 15px`,
+                      borderColor: primaryColorHover,
+                    },
+                  },
+                }}
               />
-              <DuckieTextField
-                label="Confirmar Senha"
+            </motion.div>
+            <motion.div variants={item}>
+              <CustomTextField
+                label="Senha"
                 type="password"
                 variant="filled"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                sx={{ width: "100%" }}
-              />
-              <Button
-                variant="text"
-                onClick={handleSubscribe}
+                onKeyDown={handleKeyDown}
                 sx={{
                   width: "100%",
-                  background: "#5A79D6",
+                  "& .MuiFilledInput-root": {
+                    backgroundColor: primaryColorTransparent,
+                    borderColor: primaryColor,
+                    "&:hover": {
+                      backgroundColor: primaryColorTransparent,
+                      border: "1px solid",
+                      borderColor: primaryColorHover,
+                    },
+                    "&.Mui-focused": {
+                      backgroundColor: primaryColorTransparent,
+                      boxShadow: `${primaryColor} 2px 2px 15px`,
+                      borderColor: primaryColorHover,
+                    },
+                  },
+                }}
+              />
+            </motion.div>
+            <motion.div variants={item}>
+              <Button
+                variant="contained"
+                onClick={handleSubscribe}
+                onKeyDown={handleKeyDown}
+                sx={{
+                  width: "100%",
                   padding: ".8rem",
                   borderRadius: "1.2rem",
+                  border: "1px solid",
+                  borderColor: primaryColorHover,
                   textTransform: "capitalize",
                   color: "#FFF",
+                  background: primaryColorHover,
                   "&:hover": {
-                    background: "#2947A3",
+                    background: primaryColor,
+                    border: "1px solid",
+                    borderColor: primaryColorHover,
+                    boxShadow: "none",
                   },
                 }}
               >
-                Cadastrar
+                Entrar
               </Button>
-            </Box>
-          </Box>
-        </Box>
+            </motion.div>
+            <motion.div variants={item}>
+              <Box sx={{ padding: ".5rem 0", textAlign: "center" }}>
+                <Typography variant="body1">
+                  Já tem uma conta?
+                  <Link to="/login">
+                    <Button
+                      variant="text"
+                      sx={{ textTransform: "none", color: primaryColorHover }}
+                    >
+                      Fazer Login
+                    </Button>
+                  </Link>
+                </Typography>
+              </Box>
+            </motion.div>
+          </motion.div>
+        </motion.div>
       </Container>
-    </React.Fragment>
+    </Box>
   );
 }
