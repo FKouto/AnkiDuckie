cf8a2811024c9667436c6a1bae743d7528be940d
Felipe <koutodev@gmail.com>
2024-06-01 22:24:48 -0300

    Troca da importação do icones através de uma func em HugeIcons & add de uma func para exportar os cards de LandingPage

---
+++ src/assets/images/DecksExport.js
@@ -0,0 +1,12 @@
+export const CardAzul =
+  require("../../assets/images/illustrationDeck/Card-Home-Azul.svg").ReactComponent;
+export const CardLaranja =
+  require("../../assets/images/illustrationDeck/Card-Home-Laranja.svg").ReactComponent;
+export const CardRosa =
+  require("../../assets/images/illustrationDeck/Card-Home-Rosa.svg").ReactComponent;
+export const CardRoxo =
+  require("../../assets/images/illustrationDeck/Card-Home-Roxo.svg").ReactComponent;
+export const CardVerde =
+  require("../../assets/images/illustrationDeck/Card-Home-Verde.svg").ReactComponent;
+export const CardVermelho =
+  require("../../assets/images/illustrationDeck/Card-Home-Vermelho.svg").ReactComponent;
